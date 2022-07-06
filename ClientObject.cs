e6b493e1a756be300f9cc86b8145b10af53b39c4
Gorlishchev <Gorlishchev@IT-FOXY>
2022-07-06 15:12:59 +0300

    Добавьте файлы проекта.

---
+++ ClientObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace ConsoleServer
+{
+    internal class ClientObject
+    {
+        public TcpClient Client;
+        public ClientObject(TcpClient client)
+        {
+            Client = client;
+        }
+        public void Process()
+        {
+            NetworkStream networkStream = null;
+            try
+            {
+                networkStream = Client.GetStream();
+                byte [] buffer = new byte[1024]; // Буфер для получаемых данных
+                while (true)
+                {
+                    // Получаем сообщение
+                    StringBuilder stringBuilder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = networkStream.Read(buffer, 0, buffer.Length);
+                        stringBuilder.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
+                    } 
+                    while (networkStream.DataAvailable);
+                    string message = stringBuilder.ToString();
+                    Console.WriteLine(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (networkStream != null)
+                {
+                    networkStream.Close();
+                }
+                if (Client != null)
+                {
+                    Client.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ConsoleServer
+{
+    internal class Program
+    {
+        const int port = 8888;
+        static TcpListener tcplistener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                tcplistener = new TcpListener(IPAddress.Parse("192.168.0.140"), port);
+                tcplistener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcplistener.AcceptTcpClient();
+                    ClientObject clientObject = new ClientObject(tcpClient);
+
+                    // Создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (tcplistener != null)
+                {
+                    tcplistener.Stop();
+                }
+            }
+        }
+    }
+}
